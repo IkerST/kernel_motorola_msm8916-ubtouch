6a0b726d8d594785a1b80cba2eac4f7f11204517
IkerST <ikersanditrejo@gmail.com>
2018-10-05 19:32:18 -0500

    Build: Change Toolchain

--- travis.sh
+++ travis.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 
 #Setup toolchain
-axel --num-connections=$( echo $( nproc --all) + $( nproc --all) | bc ) --output=toolchain.tar.xz https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/arm-eabi/gcc-linaro-7.3.1-2018.05-x86_64_arm-eabi.tar.xz
-tar -xf toolchain.tar.xz
-mv *arm-eabi arm-eabi
+git clone https://github.com/IkerST/toolchains.git toolchain --single-branch --depth=1 -j$(nproc --all)
+
 #Set Variables
-export CROSS_COMPILE="/usr/bin/ccache ./arm-eabi/bin/arm-eabi-" ;
-export ARCH=arm;
-export SUBARCH=arm;
-export KBUILD_BUILD_USER="ist";
-export KBUILD_BUILD_HOST="travis";
-STRIP="arm-eabi/bin/arm-eabi-strip";
-export CCOMPILE=$CROSS_COMPILE;
-export CROSS_COMPILE="/usr/bin/ccache arm-eabi-" ;
-export PATH=$PATH:./arm-eabi/bin/ ;
+TOOLCHAINDIR=$(pwd)/toolchain/linaro-7.2
+export CROSS_COMPILE="/usr/bin/ccache $TOOLCHAINDIR/bin/arm-eabi-"
+export CCOMPILE=$CROSS_COMPILE
+export ARCH=arm
+export KBUILD_BUILD_USER="ist"
+export KBUILD_BUILD_HOST="travis"
 
 #Build
-make harpia_defconfig;
-make -j$( echo $( nproc --all) + $( nproc --all) | bc );
+make harpia_defconfig
+make -j$( echo $( nproc --all) + $( nproc --all) | bc )
